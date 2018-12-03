a31d55880ff937fe27fbcebef1a097b4f99df83d
Maarten de Boer <maarten@cloudstek.nl>
2018-12-03 21:46:38 +0100

    Fix root node deprecation in symfony/config > 4.1 (#448)
    Fixes the deprecation warnings about tree builders without root nodes being deprecated since 4.2. This fix is BC with 4.1 and older and is similar to the fix applied to doctrine/DoctrineBundle.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -27,8 +27,14 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('hautelook_alice');
+        $treeBuilder = new TreeBuilder('hautelook_alice');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC for symfony/config < 4.2
+            $rootNode = $treeBuilder->root('hautelook_alice');
+        }
 
         $rootNode
             ->children()
